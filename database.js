e40673e62297c09afaef8fb80a87a80525f7abff
Ruth Johnson <ruthjohnson2013@gmail.com>
2015-04-04 22:48:09 -0700

    Create database.js

---
+++ database.js
@@ -0,0 +1,49 @@
+var openGroups=[
+//Free and For Sale
+//267375200006835,
+//UCLA Free and For Sale
+//761559070588443,
+//Sports and Athletics
+267375176673504,
+//Daily Bruin Open
+269730429771312,
+//Events and Parties
+267375190006836,
+//Class of 2015
+267494023328286,
+//Class of 2014
+267498053327883,
+//Class of 2016
+268024079941947,
+//Political Science
+267504059993949,
+//UCLA Parent ID
+267279833349705,
+//Psychology
+267496393328049,
+//UCLA Memes
+267375180006837,
+//Housing
+267375193340169,
+//Business Econ
+267516829992672,
+//Startup UCLA
+267868949957460,
+//Economics
+267494579994897,
+//Electrical Engineering
+267500559994299,
+//Communication Studies
+267501086660913,
+//Physiological Sciences
+267496856661336,
+//English
+267508943326794,
+//UCLA CLass of 2018 Roomswap
+490084807761491
+]
+
+var wordMusthave = ["free","FREE","Free"];
+var wordOptional = [];
+
+
