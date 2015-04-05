9f1c3f784f2fe658a3f90e8b6d0d61f74dc0df6a
orrblue <orrblue@users.noreply.github.com>
2015-04-05 05:52:26 -0700

    *basically* Changed request post to request status

--- getUser.js
+++ getUser.js
@@ -143,7 +143,7 @@ function getPostMessage(id) {
 
 function getPostMessage() {
 	console.log("getPostMes called");
-	FB.api('/me/posts', function(response){
+	FB.api('/me/statuses{message}', function(response){ //ADDED here status not posts
 		console.log(response.data);
 		for(var i = 0; i<=100; i++) 
 		{
