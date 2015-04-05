a7113409e6fbe7de6d8d6bb6633af774b71c396b
Ruth Johnson <ruthjohnson2013@gmail.com>
2015-04-04 22:11:19 -0700

    Update access.js

--- access.js
+++ access.js
@@ -1,10 +1,8 @@
-//original appId field 823637307672462
-
 //Apple ID login
 window.fbAsyncInit = function() 
 {
     FB.init({
-      appId      : '684343951691384',  
+      appId      : '684343951691384',
       xfbml      : true,
       status     : true, // check login status
       cookie     : true, // enable cookies to allow the server to access the session
@@ -21,8 +19,10 @@ window.fbAsyncInit = function()
 }(document, 'script', 'facebook-jssdk'));
 
 //login
+//login
+var wordOpt={value:" "};
 var permission = "email,read_friendlists,user_status,user_likes,user_relationships,user_about_me,user_birthday,friends_status,read_stream,manage_notifications,publish_actions,user_groups,user_events";
-function Login()
+function Login() //current version
 {
 	FB.login(function(response) 
 	{
@@ -30,24 +30,98 @@ function Login()
 		{
 		   	var access_token = FB.getAuthResponse()['accessToken'];
 		   	//console.log('Access Token = '+ access_token);
+		   	
+		   	//publishing 
+		   	/* make the API call */
+		   	wordOpt.value=document.getElementById("input").value;
+    			console.log(wordOpt.value);
+    				
+			FB.api("/me/feed", "POST",
+    			{
+    				
+				"message": wordOpt.value
+			 },
+				 function (response) {
+				 	console.log("Testing");
+			   if (response && !response.error) {
+			   		console.log("Testing2");
+				  /* handle the result */
+      			}
+			  }
+			); 
+		   	
 		   	FB.api('/me', function(response)
-		   	{
-		   		console.log('Good to see you, '+ response.name+ '.');
-		   	});
-		    	getUserInfo();
+		   		{
+		   			console.log('Good to see you, '+ response.name+ '.');
+		   		});
+		    //	getUserInfo();
 		    	//getPermissions();
-  			}
-  			else 
-  			{
-  	    	 		console.log('User cancelled login or did not fully authorize.');
-   			}
+  		}
+
 	},{scope: permission});
 }
-//////////////////////////////////////////////////
-function Logout()
+
+///////////////////////////////////////////////////////////////////////////
+/*function PostMessage()*/ 
+var wordOpt={value:" "};
+
+ function PostMessage()
 {
-	FB.logout(function(){document.location.reload();});
+	FB.login(function(response) 
+	{
+		if (response.authResponse) 
+		{
+		   	var access_token = FB.getAuthResponse()['accessToken'];
+		   	//console.log('Access Token = '+ access_token);
+		   	console.log("testing...");
+		   	//publishing 
+		   	wordOpt.value=document.getElementById("input").value;
+			console.log(document.getElementById("input").value);
+		   	
+		   	/* make the API call */
+			FB.api("/me/feed", "POST",
+    			{
+				  "message": wordOpt.value
+			 },
+				 function (response) {
+				 	console.log("Testing");
+			   if (response && !response.error) {
+			   		console.log("Testing2");
+				  /* handle the result */
+      			}
+			  }
+			);
+		   	
+		   	FB.api('/me', function(response)
+		   		{
+		   			console.log('Good to see you, '+ response.name+ '.');
+		   		});
+		    //	getUserInfo();
+		    	//getPermissions();
+  		}
+
+	},{scope: permission});
 }
+/////////////////////////////////////////////////
+/*
+var wordOpt={value:" "};
+
+function message()
+{	
+	wordOpt.value=document.getElementById("input").value;
+	console.log(document.getElementById("input").value);
+	FB.api('/search?q='+wordOpt.value+'&type=event', function(response) 
+	{
+		for(var r=0;r<50;r++)
+		{
+			console.log(response.data[r].id);
+			traceEvent(response.data[r].id,"forworld");
+		}	
+    	});
+    	
+}
+*/
+
 ///////////////////////////////////////////////////
 /*
 function getPermissions() 
